325c5b3a4e78d54262f6c53a93cbd32d1e446778
RubenG <rubengres@hotmail.fr>
2017-10-31 17:07:45 +0100

    Menu bien tavu

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args){
-		Game game = new Game("Nice map bruh", 1000, 400);
+		Game game = new Game("Blastoff", 1000, 400);
 		game.start();
 	}
 
